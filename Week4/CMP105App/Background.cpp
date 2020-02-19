1f09f6aff11033b49b47b572131a29363f0267e7
Mooshtie <59955493+Mooshtie@users.noreply.github.com>
2020-02-19 10:44:59 +0000

    Working!!!!
    The Camera finally stops on the edges!

--- Week4/CMP105App/Background.cpp
+++ Week4/CMP105App/Background.cpp
@@ -2,6 +2,7 @@
 
 Background::Background()
 {
+	speed = 0;
 }
 
 Background::~Background()
@@ -18,7 +19,7 @@ void Background::handleInput(float dt)
 	}
 	if (input->isMouseLDown())
 	{
-		speed = 500;
+		speed = 1000;
 		setVelocity(speed, 0);
 		move(getVelocity() * dt);
 	}
@@ -26,4 +27,13 @@ void Background::handleInput(float dt)
 
 void Background::update(float dt)
 {
+	sf::Vector2u pos = window->getSize();
+	if (getPosition().x > pos.x - 1200)
+	{
+		setPosition(0, 0);
+	}
+	if (getPosition().x + 11038 < pos.x)
+	{
+		setPosition(-9838, 0); //9838
+	}
 }
